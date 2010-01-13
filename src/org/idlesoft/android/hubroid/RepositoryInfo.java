bc3c75b995d678018d3e5ea2f37320b5eef98fa7
Eddie Ringle <eddie@eringle.net>
2010-01-12 19:04:39 -0500

    Detect if current repository is a fork and provide a button to access the forked repository.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/RepositoryInfo.java
+++ src/org/idlesoft/android/hubroid/RepositoryInfo.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -42,10 +43,39 @@ public class RepositoryInfo extends TabActivity {
 		}
 	};
 
+	private Runnable threadProc_forkedRepoInfo = new Runnable() {
+		public void run() {
+			TextView tv = (TextView)findViewById(R.id.repository_fork_of);
+			m_intent = new Intent(RepositoryInfo.this, RepositoryInfo.class);
+			m_intent.putExtra("username", tv.getText().toString().split("/")[0]);
+			m_intent.putExtra("repo_name", tv.getText().toString().split("/")[1]);
+			RepositoryInfo.this.startActivity(m_intent);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			runOnUiThread(new Runnable() {
+				public void run() {
+					m_progressDialog.dismiss();
+				}
+			});
+		}
+	};
+
 	View.OnClickListener username_onClickListener = new View.OnClickListener() {
 		public void onClick(View v) {
 			m_progressDialog = ProgressDialog.show(RepositoryInfo.this, "Please wait...", "Loading User Information...");
 			Thread thread = new Thread(null, threadProc_userInfo);
+			thread.start();
+		}
+	};
+
+	View.OnClickListener forkedRepo_onClickListener = new View.OnClickListener() {
+		public void onClick(View v) {
+			m_progressDialog = ProgressDialog.show(RepositoryInfo.this, "Please wait...", "Loading Repository Information...");
+			Thread thread = new Thread(null, threadProc_forkedRepoInfo);
 			thread.start();
 		}
 	};
@@ -89,7 +119,30 @@ public class RepositoryInfo extends TabActivity {
 				repo_fork_count.setText(m_jsonData.getString("forks"));
 				TextView repo_watcher_count = (TextView)findViewById(R.id.repository_watcher_count);
 				repo_watcher_count.setText(m_jsonData.getString("watchers"));
+
+				if (m_jsonData.getBoolean("fork") == true) {
+					// Find out what this is a fork of...
+					JSONArray network = Hubroid.make_api_request(new URL("http://github.com/api/v2/json/repos/show/"
+																		+ URLEncoder.encode(extras.getString("username"))
+																		+ "/"
+																		+ URLEncoder.encode(extras.getString("repo_name"))
+																		+ "/network")).getJSONArray("network");
+					String forked_user = network.getJSONObject(0).getString("owner");
+					String forked_repo = network.getJSONObject(0).getString("name");
+					// Show "Fork of:" label, it's value, and the button
+					TextView repo_fork_of_label = (TextView)findViewById(R.id.repository_fork_of_label);
+					repo_fork_of_label.setVisibility(0);
+					TextView repo_fork_of = (TextView)findViewById(R.id.repository_fork_of);
+					repo_fork_of.setText(forked_user + "/" + forked_repo);
+					repo_fork_of.setVisibility(0);
+					Button goto_forked_repository_btn = (Button)findViewById(R.id.goto_forked_repository_btn);
+					goto_forked_repository_btn.setVisibility(0);
+					// Set the onClick listener for the button
+					goto_forked_repository_btn.setOnClickListener(forkedRepo_onClickListener);
+				}
 			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
 
