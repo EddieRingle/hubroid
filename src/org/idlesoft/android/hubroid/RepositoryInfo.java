4280d44a080ea9fdf791a9cc9789ce7249eae5ad
Eddie Ringle <eddie@eringle.net>
2010-01-12 20:27:35 -0500

    Added Repository Network List (fork listing)
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/RepositoryInfo.java
+++ src/org/idlesoft/android/hubroid/RepositoryInfo.java
@@ -13,14 +13,20 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class RepositoryInfo extends TabActivity {
 	public ProgressDialog m_progressDialog;
 	public JSONObject m_jsonData;
 	public Intent m_intent;
+	public ForkListAdapter m_forkListAdapter;
+	public JSONArray m_jsonForkData;
+	public int m_position;
 
 	private Runnable threadProc_userInfo = new Runnable() {
 		public void run() {
@@ -79,6 +85,39 @@ public class RepositoryInfo extends TabActivity {
 		}
 	};
 
+	private Runnable threadProc_itemClick = new Runnable() {
+		public void run() {
+			try {
+	        	m_intent = new Intent(RepositoryInfo.this, RepositoryInfo.class);
+	        	m_intent.putExtra("repo_name", m_jsonForkData.getJSONObject(m_position).getString("name"));
+	        	m_intent.putExtra("username", m_jsonForkData.getJSONObject(m_position).getString("owner"));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			RepositoryInfo.this.startActivity(m_intent);
+
+			runOnUiThread(new Runnable() {
+				public void run() {
+					m_progressDialog.dismiss();
+				}
+			});
+		}
+	};
+
+	private OnItemClickListener m_onForkListItemClick = new OnItemClickListener() {
+		public void onItemClick(AdapterView parent, View v, int position, long id) {
+			m_position = position;
+			m_progressDialog = ProgressDialog.show(RepositoryInfo.this, "Please wait...", "Loading Repository's Network...", true);
+			Thread thread = new Thread(null, threadProc_itemClick);
+			thread.start();
+		}
+	};
+
 	@Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -88,7 +127,7 @@ public class RepositoryInfo extends TabActivity {
 
         tabhost.addTab(tabhost.newTabSpec("tab1").setIndicator("Repository Info").setContent(R.id.repo_info_tab));
         tabhost.addTab(tabhost.newTabSpec("tab2").setIndicator("Commit Log").setContent(R.id.repo_commits_tab));
-        tabhost.addTab(tabhost.newTabSpec("tab3").setIndicator("Forks").setContent(R.id.repo_forks_tab));
+        tabhost.addTab(tabhost.newTabSpec("tab3").setIndicator("Network").setContent(R.id.repo_forks_tab));
 
         tabhost.setCurrentTab(0);        
 
@@ -118,15 +157,16 @@ public class RepositoryInfo extends TabActivity {
 				TextView repo_watcher_count = (TextView)findViewById(R.id.repository_watcher_count);
 				repo_watcher_count.setText(m_jsonData.getString("watchers"));
 
+				m_jsonForkData = Hubroid.make_api_request(new URL("http://github.com/api/v2/json/repos/show/"
+						+ URLEncoder.encode(extras.getString("username"))
+						+ "/"
+						+ URLEncoder.encode(extras.getString("repo_name"))
+						+ "/network")).getJSONArray("network");
+
 				if (m_jsonData.getBoolean("fork") == true) {
 					// Find out what this is a fork of...
-					JSONArray network = Hubroid.make_api_request(new URL("http://github.com/api/v2/json/repos/show/"
-																		+ URLEncoder.encode(extras.getString("username"))
-																		+ "/"
-																		+ URLEncoder.encode(extras.getString("repo_name"))
-																		+ "/network")).getJSONArray("network");
-					String forked_user = network.getJSONObject(0).getString("owner");
-					String forked_repo = network.getJSONObject(0).getString("name");
+					String forked_user = m_jsonForkData.getJSONObject(0).getString("owner");
+					String forked_repo = m_jsonForkData.getJSONObject(0).getString("name");
 					// Show "Fork of:" label, it's value, and the button
 					TextView repo_fork_of_label = (TextView)findViewById(R.id.repository_fork_of_label);
 					repo_fork_of_label.setVisibility(0);
@@ -138,6 +178,11 @@ public class RepositoryInfo extends TabActivity {
 					// Set the onClick listener for the button
 					goto_forked_repository_btn.setOnClickListener(forkedRepo_onClickListener);
 				}
+
+				m_forkListAdapter = new ForkListAdapter(RepositoryInfo.this, m_jsonForkData);
+				ListView repo_list = (ListView)findViewById(R.id.repo_forks_list);
+				repo_list.setAdapter(m_forkListAdapter);
+				repo_list.setOnItemClickListener(m_onForkListItemClick);
 			} catch (JSONException e) {
 				e.printStackTrace();
 			} catch (MalformedURLException e) {
