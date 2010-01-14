2f1919afaeafacd53f89a453ccf3f68faee0624b
Eddie Ringle <eddie@eringle.net>
2010-01-13 21:12:38 -0500

    Added Repository Commit Log.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/CommitListAdapter.java
+++ src/org/idlesoft/android/hubroid/CommitListAdapter.java
@@ -17,7 +17,8 @@ public class CommitListAdapter extends BaseAdapter {
 	private LayoutInflater m_inflater;
 
 	public static class ViewHolder {
-		public TextView text;
+		public TextView commit_shortdesc;
+		public TextView commit_sha;
 	}
 
 	public CommitListAdapter(final Context context, JSONArray jsonarray) {
@@ -46,15 +47,17 @@ public class CommitListAdapter extends BaseAdapter {
 	public View getView(int index, View convertView, ViewGroup parent) {
 		ViewHolder holder;
 		if (convertView == null) {
-			convertView = m_inflater.inflate(android.R.layout.simple_list_item_1, null);
+			convertView = m_inflater.inflate(R.layout.commit_list_item, null);
 			holder = new ViewHolder();
-			holder.text = (TextView) convertView.findViewById(android.R.id.text1);
+			holder.commit_shortdesc = (TextView) convertView.findViewById(R.id.commit_list_item_shortdesc);
+			holder.commit_sha = (TextView) convertView.findViewById(R.id.commit_list_item_sha);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
 		try {
-			holder.text.setText(m_data.getJSONObject(index).getString("name"));
+			holder.commit_shortdesc.setText(m_data.getJSONObject(index).getString("message"));
+			holder.commit_sha.setText(m_data.getJSONObject(index).getString("id"));
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
--- src/org/idlesoft/android/hubroid/RepositoryInfo.java
+++ src/org/idlesoft/android/hubroid/RepositoryInfo.java
@@ -3,6 +3,8 @@ package org.idlesoft.android.hubroid;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,21 +14,29 @@ import android.app.ProgressDialog;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class RepositoryInfo extends TabActivity {
 	public ProgressDialog m_progressDialog;
 	public JSONObject m_jsonData;
 	public Intent m_intent;
 	public ForkListAdapter m_forkListAdapter;
+	public CommitListAdapter m_commitListAdapter;
 	public JSONArray m_jsonForkData;
 	public int m_position;
+	public ArrayList<String> m_branches;
+	public String m_repo_owner;
+	public String m_repo_name;
 
 	private Runnable threadProc_userInfo = new Runnable() {
 		public void run() {
@@ -118,6 +128,46 @@ public class RepositoryInfo extends TabActivity {
 		}
 	};
 
+	private Runnable threadProc_gatherCommits = new Runnable() {
+		public void run() {
+			try {
+				JSONArray commitsJSON = Hubroid.make_api_request(new URL("http://github.com/api/v2/json/commits/list/"
+																		+ URLEncoder.encode(m_repo_owner)
+																		+ "/"
+																		+ URLEncoder.encode(m_repo_name)
+																		+ "/"
+																		+ URLEncoder.encode(m_branches.get(m_position)))).getJSONArray("commits");
+				Log.d("debug1",commitsJSON.toString());
+				m_commitListAdapter = new CommitListAdapter(RepositoryInfo.this, commitsJSON);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			runOnUiThread(new Runnable() {
+				public void run() {
+					ListView commitList = (ListView)findViewById(R.id.repo_commit_log_list);
+					commitList.setAdapter(m_commitListAdapter);
+					m_progressDialog.dismiss();
+				}
+			});
+		}
+	};
+
+	private OnItemSelectedListener m_onBranchSelect = new OnItemSelectedListener() {
+		public void onItemSelected(AdapterView parent, View view, int position, long id)
+		{
+			m_position = position;
+			m_progressDialog = ProgressDialog.show(RepositoryInfo.this, "Please wait...", "Loading Repository's Commits...", true);
+			Thread thread = new Thread(null, threadProc_gatherCommits);
+			thread.start();
+		}
+
+		public void onNothingSelected(AdapterView<?> arg0) {
+		}
+	};
+
 	@Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -133,11 +183,13 @@ public class RepositoryInfo extends TabActivity {
 
         final Bundle extras = getIntent().getExtras();
         if (extras != null) {
+        	m_repo_name = extras.getString("repo_name");
+        	m_repo_owner = extras.getString("username");
 			try {
 				URL repo_query = new URL("http://github.com/api/v2/json/repos/show/"
-						+ URLEncoder.encode(extras.getString("username"))
+						+ URLEncoder.encode(m_repo_owner)
 						+ "/"
-						+ URLEncoder.encode(extras.getString("repo_name")));
+						+ URLEncoder.encode(m_repo_name));
 				m_jsonData = Hubroid.make_api_request(repo_query).getJSONObject("repository");
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
@@ -158,9 +210,9 @@ public class RepositoryInfo extends TabActivity {
 				repo_watcher_count.setText(m_jsonData.getString("watchers"));
 
 				m_jsonForkData = Hubroid.make_api_request(new URL("http://github.com/api/v2/json/repos/show/"
-						+ URLEncoder.encode(extras.getString("username"))
+						+ URLEncoder.encode(m_repo_owner)
 						+ "/"
-						+ URLEncoder.encode(extras.getString("repo_name"))
+						+ URLEncoder.encode(m_repo_name)
 						+ "/network")).getJSONArray("network");
 
 				if (m_jsonData.getBoolean("fork") == true) {
@@ -183,6 +235,26 @@ public class RepositoryInfo extends TabActivity {
 				ListView repo_list = (ListView)findViewById(R.id.repo_forks_list);
 				repo_list.setAdapter(m_forkListAdapter);
 				repo_list.setOnItemClickListener(m_onForkListItemClick);
+
+				JSONObject branchesJson = Hubroid.make_api_request(new URL("http://github.com/api/v2/json/repos/show/"
+																+ URLEncoder.encode(extras.getString("username"))
+																+ "/"
+																+ URLEncoder.encode(extras.getString("repo_name"))
+																+ "/branches")).getJSONObject("branches");
+				m_branches = new ArrayList<String>(branchesJson.length());
+				Iterator<String> keys = branchesJson.keys();
+				while (keys.hasNext()) {
+					String next_branch = keys.next();
+					if (!next_branch.contains(new String("."))) {
+						m_branches.add(next_branch);
+					}
+				}
+
+				Spinner branchesSpinner = (Spinner)findViewById(R.id.branch_select);
+				ArrayAdapter<String> branchesAdapter = new ArrayAdapter<String>(RepositoryInfo.this, android.R.layout.simple_spinner_item, m_branches);
+				branchesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				branchesSpinner.setAdapter(branchesAdapter);
+				branchesSpinner.setOnItemSelectedListener(m_onBranchSelect);
 			} catch (JSONException e) {
 				e.printStackTrace();
 			} catch (MalformedURLException e) {
