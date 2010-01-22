6d7a371eb2db85c86a499e4fe8844340e685ccdd
Eddie Ringle <eddie@eringle.net>
2010-01-21 23:58:02 -0500

    Added splash screen that doubles as a login screen.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/Hubroid.java
+++ src/org/idlesoft/android/hubroid/Hubroid.java
@@ -2,8 +2,11 @@ package org.idlesoft.android.hubroid;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -14,12 +17,20 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.TabActivity;
-import android.content.Intent;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.TabHost;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Hubroid extends TabActivity {
+public class Hubroid extends Activity {
+	public static final String PREFS_NAME = "HubroidPrefs";
+	public ProgressDialog m_progressDialog;
+
 	public static JSONObject make_api_request(URL url) {
 		JSONObject json = null;
 		HttpClient c = new DefaultHttpClient();
@@ -44,9 +55,63 @@ public class Hubroid extends TabActivity {
 		return json;
 	}
 
+	private Runnable threadProc_login = new Runnable() {
+		public void run() {
+			EditText loginBox = (EditText)findViewById(R.id.et_splash_login_user);
+			EditText tokenBox = (EditText)findViewById(R.id.et_splash_login_token);
+			String login = loginBox.getText().toString();
+			String token = tokenBox.getText().toString();
+			URL query = null;
+			try {
+				query = new URL("http://github.com/api/v2/json/user/emails?login="
+									+ URLEncoder.encode(login)
+									+ "&token="
+									+ URLEncoder.encode(token));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+			runOnUiThread(new Runnable() {
+				public void run() {
+					m_progressDialog.setMessage("Authenticating...");
+					m_progressDialog.show();
+				}
+			});
+			JSONObject result = make_api_request(query);
+			if (result == null || result.has("error")) {
+				runOnUiThread(new Runnable() {
+					public void run() {
+						m_progressDialog.dismiss();
+						Toast.makeText(Hubroid.this, "Error authenticating with server", Toast.LENGTH_LONG).show();
+					}
+				});
+			} else if(result.has("emails")) {
+				SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.putString("login", login);
+				editor.putString("token", token);
+				editor.putBoolean("isLoggedIn", true);
+				editor.commit();
+				runOnUiThread(new Runnable() {
+					public void run() {
+						m_progressDialog.dismiss();
+					}
+				});
+			}
+		}
+	};
+
+	private OnClickListener m_loginButtonClick = new OnClickListener() {
+		public void onClick(View v) {
+			Thread thread = new Thread(threadProc_login);
+			m_progressDialog = ProgressDialog.show(Hubroid.this, "Logging in...", "Initializing...");
+			thread.start();
+		}
+	};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*
         TabHost m_TabHost = getTabHost();
 
         m_TabHost.addTab(m_TabHost.newTabSpec("tab1")
@@ -55,5 +120,16 @@ public class Hubroid extends TabActivity {
         m_TabHost.addTab(m_TabHost.newTabSpec("tab2")
         					.setIndicator(getString(R.string.users_tab_label), getResources().getDrawable(R.drawable.users))
         					.setContent(new Intent(this, UsersList.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)));
+        */
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, 0);
+        boolean isLoggedIn = prefs.getBoolean("isLoggedIn", false);
+        if (!isLoggedIn) {
+        	setContentView(R.layout.splash);
+        	Button loginBtn = (Button)findViewById(R.id.btn_splash_login);
+        	loginBtn.setOnClickListener(m_loginButtonClick);
+        } else {
+        	setContentView(R.layout.splash);
+        	Toast.makeText(Hubroid.this, "Yay!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
