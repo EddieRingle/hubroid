6a2ef49360c93c5c05876aab876f4bbd1301cef6
Eddie Ringle <eddie@eringle.net>
2010-02-05 22:39:30 -0500

    Show how long ago a commit was pushed.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/CommitListAdapter.java
+++ src/org/idlesoft/android/hubroid/CommitListAdapter.java
@@ -8,6 +8,10 @@
 
 package org.idlesoft.android.hubroid;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
@@ -29,6 +33,7 @@ public class CommitListAdapter extends BaseAdapter {
 	public static class ViewHolder {
 		public TextView commit_shortdesc;
 		public ImageView gravatar;
+		public TextView commit_date;
 	}
 
 	public void loadGravatars()
@@ -79,18 +84,58 @@ public class CommitListAdapter extends BaseAdapter {
 		if (convertView == null) {
 			convertView = m_inflater.inflate(R.layout.commit_list_item, null);
 			holder = new ViewHolder();
-			holder.commit_shortdesc = (TextView) convertView.findViewById(R.id.commit_list_item_shortdesc);
+			holder.commit_date = (TextView) convertView.findViewById(R.id.tv_commit_list_item_commit_date);
+			holder.commit_shortdesc = (TextView) convertView.findViewById(R.id.tv_commit_list_item_shortdesc);
 			holder.gravatar = (ImageView) convertView.findViewById(R.id.iv_commit_list_item_gravatar);
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
 		try {
-			
+			String end;
+			SimpleDateFormat dateFormat = new SimpleDateFormat(Hubroid.GITHUB_TIME_FORMAT);
+			Date commit_time = dateFormat.parse(m_data.getJSONObject(index).getString("committed_date"));
+			Date current_time = dateFormat.parse(dateFormat.format(new Date()));
+			long ms = current_time.getTime() - commit_time.getTime();
+			long sec = ms / 1000;
+			long min = sec / 60;
+			long hour = min / 60;
+			long day = hour / 24;
+			if (day > 0) {
+				if (day == 1) {
+					end = " day ago";
+				} else {
+					end = " days ago";
+				}
+				holder.commit_date.setText(day + end);
+			} else if (hour > 0) {
+				if (hour == 1) {
+					end = " hour ago";
+				} else {
+					end = " hours ago";
+				}
+				holder.commit_date.setText(hour + end);
+			} else if (min > 0) {
+				if (min == 1) {
+					end = " minute ago";
+				} else {
+					end = " minutes ago";
+				}
+				holder.commit_date.setText(min + end);
+			} else {
+				if (sec == 1) {
+					end = " second ago";
+				} else {
+					end = " seconds ago";
+				}
+				holder.commit_date.setText(sec + end);
+			}
 			holder.gravatar.setImageBitmap(m_gravatars[index]);
 			String description = m_data.getJSONObject(index).getString("message");
 			holder.commit_shortdesc.setText(description.split("\n")[0]);
 		} catch (JSONException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
 			e.printStackTrace();
 		}
 		return convertView;
