24b81faab982e870774c60dc8330d46b3f629eb4
Eddie Ringle <eddie@eringle.net>
2010-02-10 08:34:17 -0500

    Remove unused imports.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/UserInfo.java
+++ src/org/idlesoft/android/hubroid/UserInfo.java
@@ -14,7 +14,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
