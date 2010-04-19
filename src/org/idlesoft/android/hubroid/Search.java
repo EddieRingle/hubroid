96e5417fef8fad03a690f2ddefe3e421cedd467f
Eddie Ringle <eddie@eringle.net>
2010-04-19 17:26:32 -0400

    Remove unused imports from Search.java
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/Search.java
+++ src/org/idlesoft/android/hubroid/Search.java
@@ -28,7 +28,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethod;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
