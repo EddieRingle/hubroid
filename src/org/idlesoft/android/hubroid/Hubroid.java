c2e966ee16629d574de52d9e39bf0282030b3686
Eddie Ringle <eddie@eringle.net>
2010-06-21 00:53:49 -0400

    Remove remote stack trace handling now that Google provides crash reports to developers
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/org/idlesoft/android/hubroid/Hubroid.java
+++ src/org/idlesoft/android/hubroid/Hubroid.java
@@ -50,7 +50,6 @@ import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 import com.flurry.android.FlurryAgent;
-import com.nullwire.trace.ExceptionHandler;
 
 public class Hubroid extends Activity {
 	public static final String PREFS_NAME = "HubroidPrefs";
@@ -326,8 +325,6 @@ public class Hubroid extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ExceptionHandler.register(this, "http://eringle.net/android/server.php");
 
         m_prefs = getSharedPreferences(PREFS_NAME, 0);
     	m_editor = m_prefs.edit();
