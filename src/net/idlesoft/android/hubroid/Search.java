56bbfcf16561e9c4e4dcae02bc1db0899e07a337
Eddie Ringle <eddie@eringle.net>
2010-10-03 11:38:40 -0400

    Organize imports
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/net/idlesoft/android/hubroid/Search.java
+++ src/net/idlesoft/android/hubroid/Search.java
@@ -8,25 +8,16 @@
 
 package net.idlesoft.android.hubroid;
 
-import java.io.File;
-
-import org.idlesoft.libraries.ghapi.APIAbstract.Response;
 import org.idlesoft.libraries.ghapi.GitHubAPI;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
--- src/net/idlesoft/android/hubroid/SingleActivityItem.java
+++ src/net/idlesoft/android/hubroid/SingleActivityItem.java
@@ -17,7 +17,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
