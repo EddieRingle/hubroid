2d5facc208932ac24f419cbd339b77639aec6af6
Martin Foot <martin@mfoot.com>
2011-01-26 13:29:25 +0000

    Added the clear preferences / clear cache menus to the Dashboard Activity.

--- src/net/idlesoft/android/apps/github/activities/Dashboard.java
+++ src/net/idlesoft/android/apps/github/activities/Dashboard.java
@@ -9,6 +9,7 @@
 package net.idlesoft.android.apps.github.activities;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -21,9 +22,13 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -187,5 +192,38 @@ public class Dashboard extends Activity {
         outState.putString("postLink", postLink.getText().toString());
 
         super.onSaveInstanceState(outState);
+    }
+
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        if (menu.hasVisibleItems()) {
+            menu.clear();
+        }
+        menu.add(0, 1, 0, "Clear Preferences");
+        menu.add(0, 2, 0, "Clear Cache");
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                SharedPreferences.Editor editor = getSharedPreferences(Hubroid.PREFS_NAME, 0).edit();
+                editor.clear().commit();
+
+                final Intent intent = new Intent(this, Hubroid.class);
+                startActivity(intent);
+                return true;
+            case 2:
+                final File root = Environment.getExternalStorageDirectory();
+                if (root.canWrite()) {
+                    final File hubroid = new File(root, "hubroid");
+                    if (!hubroid.exists() && !hubroid.isDirectory()) {
+                        return true;
+                    } else {
+                        hubroid.delete();
+                        return true;
+                    }
+                }
+        }
+        return false;
     }
 }
