9939bc2bf38b7350dffcd4b741c3dcd809e22cb1
Martin Foot <martin@mfoot.com>
2011-01-26 18:08:09 +0000

    Multi-file diffs should now be displayed in the WebView without the app crashing (although they need some work to look nice).

--- src/net/idlesoft/android/apps/github/activities/CommitChangeViewer.java
+++ src/net/idlesoft/android/apps/github/activities/CommitChangeViewer.java
@@ -8,11 +8,13 @@
 
 package net.idlesoft.android.apps.github.activities;
 
-import com.flurry.android.FlurryAgent;
+import java.io.File;
 
 import net.idlesoft.android.apps.github.R;
 
+import org.idlesoft.libraries.ghapi.APIAbstract.Response;
 import org.idlesoft.libraries.ghapi.GitHubAPI;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,7 +28,7 @@ import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.TextView;
 
-import java.io.File;
+import com.flurry.android.FlurryAgent;
 
 public class CommitChangeViewer extends Activity {
     private GitHubAPI mGapi = new GitHubAPI();
@@ -67,44 +69,51 @@ public class CommitChangeViewer extends Activity {
         if (extras != null) {
             mRepositoryName = extras.getString("repo_name");
             mRepositoryOwner = extras.getString("repo_owner");
+            String gitRef = extras.getString("commit_sha");
             try {
-                mJson = new JSONObject(extras.getString("json"));
+                Response commitInfo = mGapi.commits.commit(mRepositoryOwner, mRepositoryName, gitRef);
+                JSONObject mJson = new JSONObject(commitInfo.resp).getJSONObject("commit");
 
                 final WebView webView = (WebView) findViewById(R.id.wv_commitView_diff);
 
                 String content =
-                		"<style type=\"text/css\">"
-                        + "div {"
-                        + "font-family: monospace;"
-                        + "white-space: nowrap;"
-                        + "display: inline-block;"
-                        + "}"
-                        + ".lines {"
-                        + "background-color: #EAF2F5;"
-                        + "}"
-                        + ".added {"
-                        + "background-color: #DDFFDD;"
-                        + "}"
-                        + ".removed {"
-                        + "background-color: #FFDDDD;"
-                        + "}"
-                        + "</style>";
+                    "<style type=\"text/css\">"
+                    + "div {"
+                    + "font-family: monospace;"
+                    + "white-space: nowrap;"
+                    + "display: inline-block;"
+                    + "}"
+                    + ".lines {"
+                    + "background-color: #EAF2F5;"
+                    + "}"
+                    + ".added {"
+                    + "background-color: #DDFFDD;"
+                    + "}"
+                    + ".removed {"
+                    + "background-color: #FFDDDD;"
+                    + "}"
+                    + "</style>";
 
-                String[] splitDiff = mJson.getString("diff").split("\n");
-                for (int i = 0; i < splitDiff.length; i++) {
-                    if (splitDiff[i].startsWith("@@")) {
-                        splitDiff[i] = "<div class=\"lines\">".concat(splitDiff[i].concat("</div>"));
-                    } else if (splitDiff[i].startsWith("+")) {
-                        splitDiff[i] = "<div class=\"added\">".concat(splitDiff[i].concat("</div>"));
-                    } else if (splitDiff[i].startsWith("-")) {
-                        splitDiff[i] = "<div class=\"removed\">".concat(splitDiff[i].concat("</div>"));
-                    } else {
-                        splitDiff[i] = "<div>".concat(splitDiff[i].concat("</div>"));
+                // Example response:
+                // 01-26 17:52:16.637: DEBUG/martin(1606): {"message":"Passes jXHR object as third argument of prefilters and transport factories.","id":"bab8079593913dbc689404aa4e83c46b9b4c9355","author":{"email":"j@ubourg.net","login":"jaubourg","name":"jaubourg"},"parents":[{"id":"d7d64713a72c67243b279b9dcb16ae9fbb825c17"}],"tree":"0ec897dc75a1602eed81a9531982726c0baa3d7b","authored_date":"2011-01-26T08:37:08-08:00","committer":{"email":"j@ubourg.net","login":"jaubourg","name":"jaubourg"},"committed_date":"2011-01-26T08:37:08-08:00","url":"\/jquery\/jquery\/commit\/bab8079593913dbc689404aa4e83c46b9b4c9355","modified":[{"filename":"src\/ajax.js","diff":"--- a\/src\/ajax.js\n+++ b\/src\/ajax.js\n@@ -73,7 +73,7 @@ function addToPrefiltersOrTransports( structure ) {\n }\n \n \/\/Base inspection function for prefilters and transports\n-function inspectPrefiltersOrTransports( structure, options, originalOptions,\n+function inspectPrefiltersOrTransports( structure, options, originalOptions, jXHR,\n \t\tdataType \/* internal *\/, inspected \/* internal *\/ ) {\n \n \tdataType = dataType || options.dataTypes[ 0 ];\n@@ -97,7 +97,7 @@ function inspectPrefiltersOrTransports( structure, options, originalOptions,\n \t\t\t} else {\n \t\t\t\toptions.dataTypes.unshift( selection );\n \t\t\t\tselection = inspectPrefiltersOrTransports(\n-\t\t\t\t\t\tstructure, options, originalOptions, selection, inspected );\n+\t\t\t\t\t\tstructure, options, originalOptions, jXHR, selection, inspected );\n \t\t\t}\n \t\t}\n \t}\n@@ -105,7 +105,7 @@ function inspectPrefiltersOrTransports( structure, options, originalOptions,\n \t\/\/ we try the catchall dataType if not done already\n \tif ( ( executeOnly || !selection ) && !inspected[ \"*\" ] ) {\n \t\tselection = inspectPrefiltersOrTransports(\n-\t\t\t\tstructure, options, originalOptions, \"*\", inspected );\n+\t\t\t\tstructure, options, originalOptions, jXHR, \"*\", inspected );\n \t}\n \t\/\/ unnecessary when only executing (prefilters)\n \t\/\/ but it'll be ignored by the caller in that case\n@@ -565,7 +565,7 @@ jQuery.extend({\n \t\t}\n \n \t\t\/\/ Apply prefilters\n-\t\tinspectPrefiltersOrTransports( prefilters, s, options );\n+\t\tinspectPrefiltersOrTransports( prefilters, s, options, jXHR );\n \n \t\t\/\/ Uppercase the type\n \t\ts.type = s.type.toUpperCase();\n@@ -638,7 +638,7 @@ jQuery.extend({\n \t\t\t}\n \n \t\t\t\/\/ Get transport\n-\t\t\ttransport = inspectPrefiltersOrTransports( transports, s, options );\n+\t\t\ttransport = inspectPrefiltersOrTransports( transports, s, options, jXHR );\n \n \t\t\t\/\/ If no transport, we auto-abort\n \t\t\tif ( !transport ) {"}]}
+                // Cater for multiple file changes
+                JSONArray fileDiffList = mJson.getJSONArray("modified");
+                for(int j = 0; j < fileDiffList.length(); j ++){
+                    String[] splitDiff = fileDiffList.getJSONObject(j).getString("diff").split("\n");
+                    for (int i = 0; i < splitDiff.length; i++) {
+                        if (splitDiff[i].startsWith("@@")) {
+                            splitDiff[i] = "<div class=\"lines\">".concat(splitDiff[i].concat("</div>"));
+                        } else if (splitDiff[i].startsWith("+")) {
+                            splitDiff[i] = "<div class=\"added\">".concat(splitDiff[i].concat("</div>"));
+                        } else if (splitDiff[i].startsWith("-")) {
+                            splitDiff[i] = "<div class=\"removed\">".concat(splitDiff[i].concat("</div>"));
+                        } else {
+                            splitDiff[i] = "<div>".concat(splitDiff[i].concat("</div>"));
+                        }
+                        content += splitDiff[i];
                     }
-                    content += splitDiff[i];
                 }
                 webView.loadData(content, "text/html", "UTF-8");
-
             } catch (final JSONException e) {
                 e.printStackTrace();
             }
