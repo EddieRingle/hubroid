35913fda9f89404777d412c66587d80f857adafe
Eddie Ringle <eddie@eringle.net>
2011-06-06 19:34:04 -0400

    Calculate time difference between current date and Gist last updated date
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/net/idlesoft/android/apps/github/adapters/GistListAdapter.java
+++ src/net/idlesoft/android/apps/github/adapters/GistListAdapter.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.TextView;
 
+import java.util.Date;
+
 public class GistListAdapter extends ArrayListAdapter<Gist> {
     public static class ViewHolder {
         public TextView gistId;
@@ -36,74 +38,75 @@ public class GistListAdapter extends ArrayListAdapter<Gist> {
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
+
+        /* Calculate time difference between the then and now */
+        String timeEnd;
+        final Date updatedTime = mData.get(index).getUpdatedAt();
+        final Date currentTime = new Date();
+        final long ms = currentTime.getTime() - updatedTime.getTime();
+        final long sec = ms / 1000;
+        final long min = sec / 60;
+        final long hour = min / 60;
+        final long day = hour / 24;
+        final long week = day / 7;
+        final long month = week / 4;
+        final long year = day / 365;
+
+        if (year > 0) {
+            if (year == 1) {
+                timeEnd = " year ago";
+            } else {
+                timeEnd = " years ago";
+            }
+            holder.gistLastModified.setText("Updated " + year + timeEnd);
+        } else if (month > 0) {
+            if (month == 1) {
+                timeEnd = " month ago";
+            } else {
+                timeEnd = " months ago";
+            }
+            holder.gistLastModified.setText("Updated " + month + timeEnd);
+        } else if (week > 0) {
+            if (week == 1) {
+                timeEnd = " week ago";
+            } else {
+                timeEnd = " weeks ago";
+            }
+            holder.gistLastModified.setText("Updated " + week + timeEnd);
+        } else if (day > 0) {
+            if (day == 1) {
+                timeEnd = " day ago";
+            } else {
+                timeEnd = " days ago";
+            }
+            holder.gistLastModified.setText("Updated " + day + timeEnd);
+        } else if (hour > 0) {
+            if (hour == 1) {
+                timeEnd = " hour ago";
+            } else {
+                timeEnd = " hours ago";
+            }
+            holder.gistLastModified.setText("Updated " + hour + timeEnd);
+        } else if (min > 0) {
+            if (min == 1) {
+                timeEnd = " minute ago";
+            } else {
+                timeEnd = " minutes ago";
+            }
+            holder.gistLastModified.setText("Updated " + min + timeEnd);
+        } else {
+            if (sec == 1) {
+                timeEnd = " second ago";
+            } else {
+                timeEnd = " seconds ago";
+            }
+            holder.gistLastModified.setText("Updated " + sec + timeEnd);
+        }
+
         holder.gistId.setText(mData.get(index).getId());
         holder.gistDescription.setText(mData.get(index).getDescription());
-        holder.gistLastModified.setText(mData.get(index).getUpdatedAt().toString());
         holder.gistFileCount.setText(mData.get(index).getFiles().size() + " file(s)");
-        /*
-        try {
-            String end;
-            final SimpleDateFormat dateFormat = new SimpleDateFormat(
-                    Hubroid.GITHUB_ISSUES_TIME_FORMAT);
-            final Date commit_time = dateFormat.parse(mJson.getJSONObject(index).getString(
-                    "updated_at"));
-            final Date current_time = new Date();
-            final long ms = current_time.getTime() - commit_time.getTime();
-            final long sec = ms / 1000;
-            final long min = sec / 60;
-            final long hour = min / 60;
-            final long day = hour / 24;
-            final long year = day / 365;
-            if (year > 0) {
-                if (year == 1) {
-                    end = " year ago";
-                } else {
-                    end = " years ago";
-                }
-                holder.issueLastUpdatedDate.setText("Updated " + year + end);
-            }
-            if (day > 0) {
-                if (day == 1) {
-                    end = " day ago";
-                } else {
-                    end = " days ago";
-                }
-                holder.issueLastUpdatedDate.setText("Updated " + day + end);
-            } else if (hour > 0) {
-                if (hour == 1) {
-                    end = " hour ago";
-                } else {
-                    end = " hours ago";
-                }
-                holder.issueLastUpdatedDate.setText("Updated " + hour + end);
-            } else if (min > 0) {
-                if (min == 1) {
-                    end = " minute ago";
-                } else {
-                    end = " minutes ago";
-                }
-                holder.issueLastUpdatedDate.setText("Updated " + min + end);
-            } else {
-                if (sec == 1) {
-                    end = " second ago";
-                } else {
-                    end = " seconds ago";
-                }
-                holder.issueLastUpdatedDate.setText("Updated " + sec + end);
-            }
-            if (mJson.getJSONObject(index).getString("state").equalsIgnoreCase("open")) {
-                holder.issueIcon.setImageResource(R.drawable.issues_open);
-            } else {
-                holder.issueIcon.setImageResource(R.drawable.issues_closed);
-            }
-            holder.issueNumber.setText("#" + mJson.getJSONObject(index).getString("number"));
-            holder.issueTitle.setText(mJson.getJSONObject(index).getString("title"));
-        } catch (final JSONException e) {
-            e.printStackTrace();
-        } catch (final ParseException e) {
-            e.printStackTrace();
-        }
-        */
+
         return convertView;
     }
 }
