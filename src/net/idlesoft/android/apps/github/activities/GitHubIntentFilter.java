8e056590a2811fee21a1e2b795aa3879d2771669
Eddie Ringle <eddie@eringle.net>
2011-08-07 04:19:21 -0400

    Add some extra checks in the intent filter
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- src/net/idlesoft/android/apps/github/activities/GitHubIntentFilter.java
+++ src/net/idlesoft/android/apps/github/activities/GitHubIntentFilter.java
@@ -14,53 +14,66 @@ public class GitHubIntentFilter extends BaseActivity {
 		final Intent intent = new Intent();
 
 		if (parts[2].equalsIgnoreCase("github.com")) {
-			if (parts.length == 3) {
+			if (parts.length <= 3) {
 				/* User only meant to go to github.com */
-			} else if (parts.length == 4) {
-				intent.setClass(getApplicationContext(), Profile.class);
-				intent.putExtra("username", parts[3]);
-				startActivity(intent);
-			} else if (parts.length == 5) {
-				intent.setClass(getApplicationContext(), Repository.class);
-				intent.putExtra("repo_owner", parts[3]);
-				intent.putExtra("repo_name", parts[4]);
-				startActivity(intent);
-			} else if (parts.length >= 6) {
-				if (parts[5].equalsIgnoreCase("issues")) {
-					intent.setClass(getApplicationContext(), Issues.class);
-					intent.putExtra("repo_owner", parts[3]);
-					intent.putExtra("repo_name", parts[4]);
-					if (parts.length > 6 && parts[6] != null) {
-						intent.setClass(getApplicationContext(), SingleIssue.class);
-						intent.putExtra("number", Integer.parseInt(parts[6]));
-					}
+			} else {
+				if (parts[3].equalsIgnoreCase("blog")
+						|| parts[3].equalsIgnoreCase("explore")
+						|| parts[3].equalsIgnoreCase("languages")
+						|| parts[3].equalsIgnoreCase("timeline")
+						|| parts[3].equalsIgnoreCase("login")
+						|| parts[3].equalsIgnoreCase("inbox")
+						|| parts[3].equalsIgnoreCase("repositories")) {
+					/* do nothing */
+				} else if (parts[3].equalsIgnoreCase("search")) {
+					intent.setClass(getApplicationContext(), Search.class);
 					startActivity(intent);
-				} else if (parts[5].equalsIgnoreCase("network")) {
-					intent.setClass(getApplicationContext(), NetworkList.class);
-					intent.putExtra("repo_owner", parts[3]);
-					intent.putExtra("repo_name", parts[4]);
+				} else if (parts.length == 4) {
+					intent.setClass(getApplicationContext(), Profile.class);
+					intent.putExtra("username", parts[3]);
 					startActivity(intent);
-				} else if (parts[5].equalsIgnoreCase("commits")) {
-					if (parts.length > 6 && parts[6] != null) {
-						intent.setClass(getApplicationContext(), CommitsList.class);
+				} else if (parts.length == 5) {
+						intent.setClass(getApplicationContext(), Repository.class);
 						intent.putExtra("repo_owner", parts[3]);
 						intent.putExtra("repo_name", parts[4]);
-						intent.putExtra("branch_name", parts[6]);
 						startActivity(intent);
-					}
-				} else if (parts[5].equalsIgnoreCase("commit")) {
-					if (parts.length > 6 && parts[6] != null) {
-						intent.setClass(getApplicationContext(), Commit.class);
+				} else if (parts.length >= 6) {
+					if (parts[5].equalsIgnoreCase("issues")) {
+						intent.setClass(getApplicationContext(), Issues.class);
 						intent.putExtra("repo_owner", parts[3]);
 						intent.putExtra("repo_name", parts[4]);
-						intent.putExtra("commit_sha", parts[6]);
+						if (parts.length > 6 && parts[6] != null) {
+							intent.setClass(getApplicationContext(), SingleIssue.class);
+							intent.putExtra("number", Integer.parseInt(parts[6]));
+						}
+						startActivity(intent);
+					} else if (parts[5].equalsIgnoreCase("network")) {
+						intent.setClass(getApplicationContext(), NetworkList.class);
+						intent.putExtra("repo_owner", parts[3]);
+						intent.putExtra("repo_name", parts[4]);
+						startActivity(intent);
+					} else if (parts[5].equalsIgnoreCase("commits")) {
+						if (parts.length > 6 && parts[6] != null) {
+							intent.setClass(getApplicationContext(), CommitsList.class);
+							intent.putExtra("repo_owner", parts[3]);
+							intent.putExtra("repo_name", parts[4]);
+							intent.putExtra("branch_name", parts[6]);
+							startActivity(intent);
+						}
+					} else if (parts[5].equalsIgnoreCase("commit")) {
+						if (parts.length > 6 && parts[6] != null) {
+							intent.setClass(getApplicationContext(), Commit.class);
+							intent.putExtra("repo_owner", parts[3]);
+							intent.putExtra("repo_name", parts[4]);
+							intent.putExtra("commit_sha", parts[6]);
+							startActivity(intent);
+						}
+					} else if (parts[5].equalsIgnoreCase("branches")) {
+						intent.setClass(getApplicationContext(), BranchesList.class);
+						intent.putExtra("repo_owner", parts[3]);
+						intent.putExtra("repo_name", parts[4]);
 						startActivity(intent);
 					}
-				} else if (parts[5].equalsIgnoreCase("branches")) {
-					intent.setClass(getApplicationContext(), BranchesList.class);
-					intent.putExtra("repo_owner", parts[3]);
-					intent.putExtra("repo_name", parts[4]);
-					startActivity(intent);
 				}
 			}
 		} else if (parts[2].equalsIgnoreCase("gist.github.com")) {
