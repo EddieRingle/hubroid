bb04be577956676b74b15994ebcd73fb9696b3d9
Eddie Ringle <eddie@eringle.net>
2012-02-25 12:06:21 -0500

    Delete everything for the rewrite
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

---
+++ src/net/idlesoft/android/apps/github/Main.java
@@ -0,0 +1,15 @@
+package net.idlesoft.android.apps.github;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
